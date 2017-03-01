2c5e7aa8a5541e820c1f8eacc5b6968b7c3af0a8
Nikolay Sedelnikov <n.sedelnikov@gmail.com>
2017-03-01 20:40:04 +0300

    marked register fixture as autouse

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from imgrabber.register import Register
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def register():
     """ Return Register() instance and clear it on teardown """
     r = Register()
