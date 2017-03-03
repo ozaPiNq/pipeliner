66e0910f7cfdc90ca9d966597a27aa8a38437d78
Nikolay Sedelnikov <n.sedelnikov@gmail.com>
2017-03-03 13:05:37 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pipeliner",
-    version="0.0.1",
+    version="0.0.2",
     author="Nikolay Sedelnikov",
     author_email="n.sedelnikov@gmail.com",
     packages=find_packages(),
